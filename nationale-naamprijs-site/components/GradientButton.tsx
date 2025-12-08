88b54259c39d0602dd9996a7ed2ac2404347af18
FHulsmann <info@namarama.nl>
2025-12-08 14:56:37 +0100

    Delete nationale-naamprijs-site/components directory

--- nationale-naamprijs-site/components/GradientButton.tsx
+++
@@ -1,7 +0,0 @@
-export function GradientButton({ href = "#", children }: { href?: string; children: React.ReactNode }) {
-  return (
-    <a href={href} className="rounded-2xl bg-gradient-to-r from-[#FFB800] to-[#E73331] px-6 py-3 font-semibold text-white shadow-sm hover:opacity-90">
-      {children}
-    </a>
-  );
-}
